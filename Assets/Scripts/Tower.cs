214d88453036741d5d21a0ad69be1b66b2d63d2f
r-truesdale <rebecca.truesdale@gmail.com>
2023-10-24 12:54:43 +0100

    Tower update
    copied main scene to make my changes on, added tower and enemy tag, tower will detect enemy in range, check for closest enemy and assign that enemy as a target

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    //the waypoint of the target
+    public Transform target;
+    //range of the tower
+    public float range = 15f;
+    //creates the tag for enemies
+    public string enemyTag = "Enemy";
+
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        //finds objects assigned with enemy tag and store in array
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        // finds the enemy with the shortest distance to the tower
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        //loops through the objects tagged enemy in the enemies array
+        foreach (GameObject enemy in enemies)
+        {
+            //returns distance from tower to each enemy
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            //if the enemy has the shortest distance to the tower it is now the nearest 
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        //set the nearest enemy as a target
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
+    }
+    
+    void Update()
+    {
+        if (target == null)
+        return;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        //creates red range sphere in the scene
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
