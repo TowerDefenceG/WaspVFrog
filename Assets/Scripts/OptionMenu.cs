2170507b4baed2767e28c6d394fc3f99538fc26a
Jenny Ou <f.ou@se21.qmul.ac.uk>
2024-01-07 20:04:58 +0000

    option menu all fixed

---
+++ Assets/Scripts/OptionMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionMenu : MonoBehaviour
+{
+    // public GameObject backButton;
+    // public GameObject retryButton;
+    // public GameObject levelSelectBtn;
+    // public GameObject continueBtn;
+    public GameObject ui;
+
+
+    public void Retry()
+    {
+        PlayerStats.Rounds = 0;
+        Debug.Log(PlayerStats.Rounds);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); //change to next level later
+        Time.timeScale = 1f;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(1);
+        // Time.timeScale = 1f;
+    }
+
+    public void Back(){
+        Time.timeScale = 1f;
+        ui.SetActive(false);
+    }
+}
