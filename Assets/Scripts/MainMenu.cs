86afd52b28fd4ded6afa29544ef689c1d15d1c6f
OliviaStaniaszek <95420080+OliviaStaniaszek@users.noreply.github.com>
2024-01-07 19:15:32 +0000

    level 3 design + map level select
    + wasp particle effect

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,6 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(9);
     }
 }
