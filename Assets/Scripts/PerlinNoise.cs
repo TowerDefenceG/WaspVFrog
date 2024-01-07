88ddd543e5546e5d4d9b2fb1be1096428fbc8657
OliviaStaniaszek <95420080+OliviaStaniaszek@users.noreply.github.com>
2024-01-07 23:17:36 +0000

    lvl 3 design tweak + others
    + varied wasp nest colour
    + moved old scenes
    + WIP dappled lighting

--- Assets/Scripts/PerlinNoise.cs
+++ Assets/Scripts/PerlinNoise.cs
@@ -1,34 +1,45 @@
 using UnityEngine;
 
+// tutorial: https://www.youtube.com/watch?v=bG0uEXV6aHQ
+
 public class PerlinNoise : MonoBehaviour{
     public int width = 256;
     public int height = 256;
-    public float scale = 20f;
+    public float scale = 20;
 
-    void Start()
-    {
-        GenerateTexture();
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    public Texture2D texture = new Texture2D(width, height);
+    
+
+    void Update(){
+        // Renderer renderer = GetComponent<Renderer>();
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+
+        renderer.material.mainTexture = GenerateTexture();
     }
 
-    void GenerateTexture()
-    {
-        Texture2D perlinTexture = new Texture2D(width, height);
-
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                float xCoord = (float)x / width * scale;
-                float yCoord = (float)y / height * scale;
-
-                float perlinValue = Mathf.PerlinNoise(xCoord, yCoord);
-                perlinTexture.SetPixel(x, y, new Color(perlinValue, perlinValue, perlinValue));
+    Texture2D GenerateTexture(){
+        // generate Perlin noise map 
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
             }
         }
+        
+        texture.Apply();  //applies the texture
+        return texture; 
+    }
 
-        perlinTexture.Apply();
+    Color CalculateColor(int x, int y){
+        //convert 1-256 to 0-1 range
+        float xCoord = (float)x / width * scale + offsetX; 
+        float yCoord = (float)y / height * scale + offsetY;
 
-        // Assign the generated texture to a material
-        GetComponent<Renderer>().material.mainTexture = perlinTexture;
+
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
     }
 }
