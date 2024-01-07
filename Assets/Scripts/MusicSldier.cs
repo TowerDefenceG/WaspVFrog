287433effa1ec3dd0f932a91dea488a1b757f14a
Rebecca Truesdale <rebecca.truesdale@gmail.com>
2024-01-07 19:51:03 +0000

    Audio update
    pause menu audio buttons all work including mute button feature with correct images

--- Assets/Scripts/MusicSldier.cs
+++ Assets/Scripts/MusicSldier.cs
@@ -6,11 +6,27 @@ using UnityEngine.UI;
 public class MusicSldier : MonoBehaviour
 {
     [SerializeField] private Slider _musicslider;
+    [SerializeField] private Button musicOn;
+    [SerializeField] private Button musicOff;
+    [SerializeField] private GameObject musicOnImg;
+    [SerializeField] private GameObject musicOffImg;
     // Start is called before the first frame update
     void Start()
     {
+        musicOn.enabled = true;
+        musicOnImg.gameObject.SetActive(true);
         _musicslider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeMusicVolume(val));
     }
 
+    public void Update(){
+        if (_musicslider.value == 0){
+            musicOffImg.gameObject.SetActive(true);
+            musicOnImg.gameObject.SetActive(false);
+        }
+        else{
+        musicOnImg.gameObject.SetActive(true);
+        musicOffImg.gameObject.SetActive(false);
+        }
+    }
 
 }
--- Assets/Scripts/effectsSlider.cs
+++ Assets/Scripts/effectsSlider.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class effectsSlider : MonoBehaviour
-{
-    [SerializeField] private Slider _effectsslider;
+{   [SerializeField] private Slider _effectsslider;
+    [SerializeField] private Button effectsOn;
+    [SerializeField] private Button effectsOff;
+    [SerializeField] private GameObject effectsOnImg;
+    [SerializeField] private GameObject effectsOffImg;
     // Start is called before the first frame update
     void Start()
     {
+        effectsOn.enabled = true;
+        effectsOnImg.gameObject.SetActive(true);
         _effectsslider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeEffectsVolume(val));
     }
-
+    public void Update(){
+        if (_effectsslider.value == 0){
+            effectsOffImg.gameObject.SetActive(true);
+            effectsOnImg.gameObject.SetActive(false);
+        }
+        else{
+        effectsOnImg.gameObject.SetActive(true);
+        effectsOffImg.gameObject.SetActive(false);
+        }
+    }
 
 }
 
