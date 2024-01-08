6058612cf716d85c1473963b65b906349f613242
r-truesdale <rebecca.truesdale@gmail.com>
2024-01-08 09:30:06 +0000

    Update MusicSldier.cs comments

--- Assets/Scripts/MusicSldier.cs
+++ Assets/Scripts/MusicSldier.cs
@@ -11,14 +11,14 @@ public class MusicSldier : MonoBehaviour
     [SerializeField] private GameObject musicOnImg;
     [SerializeField] private GameObject musicOffImg;
     // Start is called before the first frame update
-    void Start()
+    void Start()//starting with musicOn button and image being active
     {
         musicOn.enabled = true;
         musicOnImg.gameObject.SetActive(true);
-        _musicslider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeMusicVolume(val));
+        _musicslider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeMusicVolume(val));//checking for slider value and changing volume to match
     }
 
-    public void Update(){
+    public void Update(){//when the slider is at 0 the img swithces to the musicoff
         if (_musicslider.value == 0){
             musicOffImg.gameObject.SetActive(true);
             musicOnImg.gameObject.SetActive(false);
