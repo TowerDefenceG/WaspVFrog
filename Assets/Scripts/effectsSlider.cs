4e480c0852d4d0d603c0811b48e80db4a4ca7e94
r-truesdale <rebecca.truesdale@gmail.com>
2024-01-08 09:24:40 +0000

    Update effectsSlider.cs comments
    added comments

--- Assets/Scripts/effectsSlider.cs
+++ Assets/Scripts/effectsSlider.cs
@@ -10,14 +10,14 @@ public class effectsSlider : MonoBehaviour
     [SerializeField] private GameObject effectsOnImg;
     [SerializeField] private GameObject effectsOffImg;
     // Start is called before the first frame update
-    void Start()
+    void Start()//defaults effects buttons to on
     {
         effectsOn.enabled = true;
         effectsOnImg.gameObject.SetActive(true);
-        _effectsslider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeEffectsVolume(val));
+        _effectsslider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeEffectsVolume(val));//checking for slider change and changing volume to match
     }
     public void Update(){
-        if (_effectsslider.value == 0){
+        if (_effectsslider.value == 0){//switches to the no sound button image when the slider is 0
             effectsOffImg.gameObject.SetActive(true);
             effectsOnImg.gameObject.SetActive(false);
         }
